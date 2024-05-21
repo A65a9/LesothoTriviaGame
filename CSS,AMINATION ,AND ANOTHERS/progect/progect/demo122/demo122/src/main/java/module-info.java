ac7ee4256791d1606320c507263e27694d762b39
TriviaGame <thabangmoeketsi820@gmail.com>
2024-05-21 11:59:45 +0200

    second commit

--- CSS,AMINATION ,AND ANOTHERS/progect/progect/demo122/demo122/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.demo122 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.media;
-
-
-    opens com.example.demo122 to javafx.fxml;
-    exports com.example.demo122;
-}
